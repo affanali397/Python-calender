5035425afcfc92c2156402088c4f5a71c0ad6f57
affanali397 <54842037+affanali397@users.noreply.github.com>
2020-02-17 15:56:02 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,12 @@
+import calendar
+
+
+def printcalendar(year):
+    # printing calendar
+    print(calendar.calendar(2020))
+
+
+# driver program to test above function
+year = 2020
+printcalendar(year)
+
